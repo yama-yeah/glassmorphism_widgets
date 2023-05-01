738b3be277c39e7fa418f4d529bb2beb1ee690ae
yama-yeah <b1021091@fun.ac.jp>
2023-05-01 19:57:24 +0900

    add golden test

--- test/glassmorphism_widgets_test.dart
+++ test/glassmorphism_widgets_test.dart
@@ -1,10 +1,168 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:glassmorphism_widgets/glassmorphism_widgets.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GlassApp(
+      theme: GlassThemeData(
+        blur: 10,
+      ),
+      home: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Colors.pinkAccent,
+      appBar: GlassAppBar(
+        title: const GlassText(
+          'Glass Morphic Demo',
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+        actions: [
+          IconButton(
+            icon: const GlassIcon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const GlassIcon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GlassListTile(
+            leading: const GlassIcon(Icons.search),
+            title: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                border: InputBorder.none,
+                hintStyle: glassTextStyle,
+              ),
+              style: glassTextStyle,
+            ),
+            onTap: () {},
+          ),
+          const GlassIcon(Icons.add),
+          const Center(
+            child: Center(
+              child: GlassContainer(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: GlassText("Hello World"),
+                ),
+              ),
+            ),
+          ),
+          const GlassIcon(Icons.add),
+          GlassButton(
+              blur: 10,
+              linearGradient: LinearGradient(
+                colors: [
+                  Colors.blue.withOpacity(0.5),
+                  Colors.blue.withOpacity(0.4),
+                ],
+              ),
+              onPressed: () {
+                showGlassBottomSheet(
+                  blur: 2,
+                  context: context,
+                  child: const Center(
+                    child: GlassText("Hello World", fontSize: 20),
+                  ),
+                );
+              },
+              child: const GlassText("Button")),
+        ],
+      ),
+      floatingActionButton: GlassFloatingActionButton(
+        onPressed: () {
+          setState(() {
+            index++;
+          });
+        },
+        child: GlassText("$index"),
+      ),
+      bottomNavigationBar: GlassBottomBar(
+        items: [
+          GlassBottomBarItem(
+            icon: const Icon(Icons.home),
+            title: const GlassText("Home"),
+            selectedGradient: LinearGradient(
+              colors: [
+                Colors.blue.withOpacity(0.5),
+                Colors.yellow.withOpacity(0.5),
+                Colors.red.withOpacity(0.5),
+              ],
+            ),
+          ),
+          GlassBottomBarItem(
+            icon: const Icon(Icons.search),
+            title: const GlassText("Search"),
+          ),
+          GlassBottomBarItem(
+            icon: const Icon(Icons.settings),
+            title: const GlassText("Settings"),
+          ),
+        ],
+        onTap: (i) {
+          setState(() {
+            index = i;
+          });
+        },
+        currentIndex: index,
+      ),
+    );
+  }
+}
 
 void main() {
-  test('adds one to input values', () {
-    //final calculator = Calculator();
-    //expect(calculator.addOne(2), 3);
-    //expect(calculator.addOne(-7), -6);
-    //expect(calculator.addOne(0), 1);
-  });
+  const targetWidget = MyApp();
+
+  testGoldens(
+    'my_app',
+    (WidgetTester tester) async {
+      await loadAppFonts();
+      await tester.pumpWidgetBuilder(targetWidget);
+      await multiScreenGolden(tester, 'my_app_screen_test', devices: [
+        Device.iphone11,
+        Device.phone,
+      ]);
+    },
+  );
 }
