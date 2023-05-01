10c43096d78eb01871c3969f522d366fd1d004f7
yama-yeah <b1021091@fun.ac.jp>
2023-05-02 00:12:44 +0900

    fix:did not fit size of app bar and list tile

--- lib/src/glass_app_bar.dart
+++ lib/src/glass_app_bar.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'glass_container.dart';
 
-/// A GlassAppBar is a Material Design app bar that is like a [AppBar] but
-/// with glass effect.
-class GlassAppBar extends StatelessWidget implements PreferredSizeWidget {
+/// A GlassAppBar is a Glassmorphic Design app bar that is like a [AppBar]
+
+class GlassAppBar extends StatefulWidget implements PreferredSizeWidget {
   const GlassAppBar({
     Key? key,
     this.leading,
@@ -36,22 +36,88 @@ class GlassAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Size preferredSize;
 
   @override
+  State<GlassAppBar> createState() => _GlassAppBarState();
+}
+
+class _GlassAppBarState extends State<GlassAppBar> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      MediaQuery.of(context).size;
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GlassContainer(
-      radius: radius ?? 0,
-      border: border,
-      blur: blur,
-      linearGradient: linearGradient,
-      borderGradient: borderGradient,
-      borderRadius: borderRadius,
+      radius: widget.radius ?? 0,
+      border: widget.border,
+      blur: widget.blur,
+      linearGradient: widget.linearGradient,
+      borderGradient: widget.borderGradient,
+      borderRadius: widget.borderRadius,
       child: AppBar(
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
-        leading: leading,
-        title: title,
-        centerTitle: centerTitle,
-        actions: actions,
+        leading: widget.leading,
+        title: widget.title,
+        centerTitle: widget.centerTitle,
+        actions: widget.actions,
       ),
     );
   }
 }
+
+// class GlassAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   const GlassAppBar({
+//     Key? key,
+//     this.leading,
+//     this.title,
+//     this.centerTitle,
+//     this.radius,
+//     this.border,
+//     this.blur,
+//     this.linearGradient,
+//     this.borderGradient,
+//     this.borderRadius,
+//     this.actions,
+//   })  : preferredSize = const Size.fromHeight(kToolbarHeight),
+//         super(key: key);
+
+//   final double? radius;
+//   final double? border;
+//   final double? blur;
+//   final LinearGradient? linearGradient;
+//   final LinearGradient? borderGradient;
+//   final BorderRadius? borderRadius;
+//   final List<Widget>? actions;
+
+//   final Widget? leading;
+//   final Widget? title;
+//   final bool? centerTitle;
+
+//   @override
+//   final Size preferredSize;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final size = MediaQuery.of(context).size;
+//     return GlassContainer(
+//       radius: radius ?? 0,
+//       border: border,
+//       blur: blur,
+//       linearGradient: linearGradient,
+//       borderGradient: borderGradient,
+//       borderRadius: borderRadius,
+//       child: AppBar(
+//         backgroundColor: Colors.transparent,
+//         shadowColor: Colors.transparent,
+//         leading: leading,
+//         title: title,
+//         centerTitle: centerTitle,
+//         actions: actions,
+//       ),
+//     );
+//   }
+// }
--- lib/src/glass_list_tile.dart
+++ lib/src/glass_list_tile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'glass_container.dart';
 
-class GlassListTile extends StatelessWidget {
+/// GlassListTile is a create ListTile with a glass effect.
+class GlassListTile extends StatefulWidget {
   const GlassListTile({
     Key? key,
     this.radius,
@@ -32,26 +33,94 @@ class GlassListTile extends StatelessWidget {
   final void Function()? onTap;
 
   @override
+  _GlassListTileState createState() => _GlassListTileState();
+}
+
+class _GlassListTileState extends State<GlassListTile> {
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      MediaQuery.of(context).size;
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GlassContainer(
-      radius: radius,
-      linearGradient: linearGradient,
-      borderGradient: borderGradient,
-      border: border,
-      blur: blur,
-      borderRadius: borderRadius,
+      radius: widget.radius,
+      linearGradient: widget.linearGradient,
+      borderGradient: widget.borderGradient,
+      border: widget.border,
+      blur: widget.blur,
+      borderRadius: widget.borderRadius,
       child: Center(
         child: Material(
           color: Colors.transparent,
           child: ListTile(
-            leading: leading,
-            title: title,
-            subtitle: subtitle,
-            trailing: trailing,
-            onTap: onTap,
+            leading: widget.leading,
+            title: widget.title,
+            subtitle: widget.subtitle,
+            trailing: widget.trailing,
+            onTap: widget.onTap,
           ),
         ),
       ),
     );
   }
 }
+
+// class GlassListTile extends StatelessWidget {
+//   const GlassListTile({
+//     Key? key,
+//     this.radius,
+//     this.border,
+//     this.blur,
+//     this.linearGradient,
+//     this.borderGradient,
+//     this.borderRadius,
+//     this.leading,
+//     this.title,
+//     this.subtitle,
+//     this.trailing,
+//     this.onTap,
+//   }) : super(key: key);
+
+//   final double? radius;
+//   final double? border;
+//   final double? blur;
+//   final LinearGradient? linearGradient;
+//   final LinearGradient? borderGradient;
+//   final BorderRadius? borderRadius;
+
+//   final Widget? leading;
+//   final Widget? title;
+//   final Widget? subtitle;
+//   final Widget? trailing;
+
+//   final void Function()? onTap;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GlassContainer(
+//       radius: radius,
+//       linearGradient: linearGradient,
+//       borderGradient: borderGradient,
+//       border: border,
+//       blur: blur,
+//       borderRadius: borderRadius,
+//       child: Center(
+//         child: Material(
+//           color: Colors.transparent,
+//           child: ListTile(
+//             leading: leading,
+//             title: title,
+//             subtitle: subtitle,
+//             trailing: trailing,
+//             onTap: onTap,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
